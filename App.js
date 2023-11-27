edca6747d55f19a62e675fc177183a1f333c7297
tj60647 <tjm@tjmcleish.com>
2023-11-26 16:15:12 -0800

    Wroking Localhost ZW Chat connection

--- App.js
+++ App.js
@@ -1,55 +1,77 @@
-/*
-Node + Express Example code for CS160 Summer 2022
-Prepared by Shm Garanganao Almeda 
+require('dotenv').config(); // At the top of your file
 
-Code referenced from: 
-https://www.digitalocean.com/community/tutorials/how-to-create-a-web-server-in-node-js-with-the-http-module"
-https://expressjs.com/en/starter/hello-world.html
-https://codeforgeek.com/render-html-file-expressjs/
-https://stackoverflow.com/questions/32257736/app-use-express-serve-multiple-html
-
-Photo Credits:
-Bunny by Satyabratasm on Unsplash <https://unsplash.com/photos/u_kMWN-BWyU>
-*/
-
-//Node modules to *require*
-//if these cause errors, be sure you've installed them, ex: 'npm install express'
 const express = require('express');
-const router = express.Router();
-const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
 const path = require('path');
 
-//specify that we want to run our website on 'http://localhost:8000/'
-const host = 'localhost';
+const app = express();
+
 const port = 8000;
 
-var publicPath = path.join(__dirname, 'public'); //get the path to use our "public" folder where we stored our html, css, images, etc
-app.use(express.static(publicPath));  //tell express to use that folder
+// Serve static files from the "public" directory
+var publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+// Use body-parser to parse incoming JSON requests
+app.use(bodyParser.json());
 
 
+app.post('/chat', async (req, res) => {
+    // Construct the payload for the external API
+    const payload = {
+        // Your payload structure...
+        data: req.body.data,
+        session_id: req.body.session_id,
+        stateful: req.body.stateful,
+        user_id: req.body.user_id
+    };
 
-//here's where we specify what to send to users that connect to our web server...
-//if there's no url extension, it will show "index.html"
-router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/"));
+    try {
+        const apiResponse = await axios.post('https://api.zerowidth.ai/process/ZO3Hxr4tQQvKDUugRQ9I/1ZCoYea03WPAJSJycaXu', payload, {
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.BEARER_TOKEN}` // Use the token here
+            }
+        });
+
+        // Send the response data back to the client
+        res.json(apiResponse.data);
+    } catch (error) {
+        console.error('Error calling external API:', error);
+        res.status(500).send('Error processing request');
+    }
 });
 
-//depending on what url extension the user navigates to, send them the respective html file. 
-app.get('/a', function (req, res) {
-    res.sendFile(publicPath + '/a.html');
-});
-app.get('/b', function (req, res) {
-    res.sendFile(publicPath + '/b.html');
-});
-app.get('/c', function (req, res) {
-    res.sendFile(publicPath + '/c.html');
+// Start the server
+app.listen(port, () => {
+    console.log(`Chatbot server running on http://localhost:${port}`);
 });
 
 
-//run this server by entering "node App.js" using your command line. 
-   app.listen(port, () => {
-     console.log(`Server is running on http://${host}:${port}`);
-   });
+///mock api response
+/*
+app.post('/chat', async (req, res) => {
+    try {
+        console.log("Received message:", req.body);
 
+        // Instead of calling an external API, just return a mock response
+        const mockApiResponse = {
+            data: {
+                message: {
+                    content: "This is a mock response",
+                    role: "bot"
+                }
+            },
+            session_id: "testSessionID",
+            stateful: true,
+            user_id: "testUserID"
+        };
 
-
+        res.json(mockApiResponse);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Error processing request');
+    }
+});
+*/
